ecc81a886b5c90be3ab68495fb178f5b5ad01a05
Michael Lodder <redmike7@gmail.com>
2022-05-30 13:43:46 -0600

    add test
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/credential/schema.rs
+++ src/credential/schema.rs
@@ -48,3 +48,28 @@ impl ClaimSchema {
         Some(result)
     }
 }
+
+#[test]
+fn test_serialize() {
+    let string = r#"{"id":"63e8b522-3ef6-4c45-92f1-47cad2449523","label":"Finclusive KYC Schema","description":"","blind_claims":[],"claim_indices":{"address1":3,"address2":4,"city":5,"credential_id":0,"date_of_birth":9,"document_content":19,"document_file_name":18,"document_id":14,"document_identification_number":16,"document_iso_country_code":20,"document_type":17,"document_url":15,"email_address":12,"first_name":1,"iso_country_code":8,"last_name":2,"phone_number":10,"phone_number_type":11,"postal_cost":7,"state":6,"tax_id_number":13},"claims":[{"claim_type":4,"label":"credential_id","print_friendly":false},{"claim_type":1,"label":"first_name","print_friendly":true,"validators":[{"Length":{"max":64}}]},{"claim_type":1,"label":"last_name","print_friendly":true,"validators":[{"Length":{"max":64}}]},{"claim_type":1,"label":"address1","print_friendly":true},{"claim_type":1,"label":"address2","print_friendly":true},{"claim_type":1,"label":"city","print_friendly":true},{"claim_type":1,"label":"state","print_friendly":true},{"claim_type":1,"label":"postal_code","print_friendly":true},{"claim_type":1,"label":"iso_country_code","print_friendly":true},{"claim_type":2,"label":"date_of_birth","print_friendly":true,"validators":[{"Range":{"min":0,"max":65000}}]},{"claim_type":1,"label":"phone_number","print_friendly":true,"validators":[{"Regex":"\\d{10,15}"}]},{"claim_type":2,"label":"phone_number_type","print_friendly":true},{"claim_type":1,"label":"email_address","print_friendly":true},{"claim_type":1,"label":"tax_id_number","print_friendly":true},{"claim_type":1,"label":"document_id","print_friendly":true},{"claim_type":1,"label":"document_url","print_friendly":true},{"claim_type":1,"label":"document_identification_number","print_friendly":true},{"claim_type":1,"label":"document_type","print_friendly":true},{"claim_type":1,"label":"document_file_name","print_friendly":true},{"claim_type":1,"label":"document_content","print_friendly":false},{"claim_type":2,"label":"document_iso_country_code","print_friendly":true}]}"#;
+    let res = serde_json::from_str::<CredentialSchema>(string);
+    assert!(res.is_ok());
+    let schema = res.unwrap();
+    assert_eq!("63e8b522-3ef6-4c45-92f1-47cad2449523", schema.id);
+    let res = serde_cbor::to_vec(&schema);
+    assert!(res.is_ok());
+    let bytes = res.unwrap();
+    let res = serde_cbor::from_slice::<CredentialSchema>(&bytes);
+    assert!(res.is_ok());
+    let schema = res.unwrap();
+    assert_eq!("63e8b522-3ef6-4c45-92f1-47cad2449523", schema.id);
+
+
+    // let res = serde_bare::to_vec(&schema);
+    // assert!(res.is_ok());
+    // let bytes = res.unwrap();
+    // let res = serde_bare::from_slice::<CredentialSchema>(&bytes);
+    // assert!(res.is_ok(), "{:?}", res);
+    // let schema = res.unwrap();
+    // assert_eq!("63e8b522-3ef6-4c45-92f1-47cad2449523", schema.id);
+}
