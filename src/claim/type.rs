63d4bbf9eb4744562b3f1682b87f64ba1cafcded
Michael Lodder <redmike7@gmail.com>
2022-12-12 13:03:08 -0700

    better claim type for human friendly formats
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/claim/type.rs
+++ src/claim/type.rs
@@ -1,7 +1,9 @@
-use serde_repr::{Deserialize_repr, Serialize_repr};
+use std::fmt::{Display, Error as FmtError, Formatter};
+use std::str::FromStr;
+use serde::{Serialize, Serializer, Deserialize, Deserializer, de::{Visitor, Error as DError, Unexpected}};
 
 /// The claim type
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Deserialize_repr, Serialize_repr)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[repr(u8)]
 pub enum ClaimType {
     /// The case where its none of the others
@@ -18,6 +20,34 @@ pub enum ClaimType {
     Enumeration = 5,
 }
 
+impl FromStr for ClaimType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+           "enumeration" => Ok(Self::Enumeration),
+            "hashed" => Ok(Self::Hashed),
+            "number" => Ok(Self::Number),
+            "scalar" => Ok(Self::Scalar),
+            "revocation" => Ok(Self::Revocation),
+            _ => Err("invalid type".to_string())
+        }
+    }
+}
+
+impl Display for ClaimType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Enumeration => write!(f, "Enumeration"),
+            Self::Hashed => write!(f, "Hashed"),
+            Self::Number => write!(f, "Number"),
+            Self::Revocation => write!(f, "Revocation"),
+            Self::Scalar => write!(f, "Scalar"),
+            _ => Err(FmtError)
+        }
+    }
+}
+
 impl From<u8> for ClaimType {
     fn from(v: u8) -> Self {
         match v {
@@ -27,6 +57,46 @@ impl From<u8> for ClaimType {
             4 => Self::Revocation,
             5 => Self::Enumeration,
             _ => Self::Unknown,
+        }
+    }
+}
+
+impl Serialize for ClaimType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        if serializer.is_human_readable() {
+            let s = self.to_string();
+            serializer.serialize_str(&s)
+        } else {
+            let u = *self as u8;
+            serializer.serialize_u8(u)
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for ClaimType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        struct ClaimTypeVisitor;
+
+        impl<'de> Visitor<'de> for ClaimTypeVisitor {
+            type Value = ClaimType;
+
+            fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                write!(formatter, "a string or byte")
+            }
+
+            fn visit_u8<E>(self, v: u8) -> Result<Self::Value, E> where E: DError {
+                Ok(ClaimType::from(v))
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: DError {
+                ClaimType::from_str(v).map_err(|_e| DError::invalid_type(Unexpected::Other(v), &self))
+            }
+        }
+
+        if deserializer.is_human_readable() {
+            deserializer.deserialize_str(ClaimTypeVisitor)
+        } else {
+            deserializer.deserialize_u8(ClaimTypeVisitor)
         }
     }
 }
